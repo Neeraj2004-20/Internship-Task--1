55e8f98def1a854002f2b9fa5e4b1ba5e57c3cb6
Neeraj <neeraj@example.com>
2026-07-24 20:41:13 +0530

    Make collectstatic non-blocking for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -m pip install --upgrade pip --break-system-packages
 python -m pip install -r requirements.txt --break-system-packages
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --ignore=*.pyc || true
